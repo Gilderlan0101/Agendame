650059a1bbe17bf9189f9e871a352f0bc61ae860
Gilderlan0101 <lansilva007gg@gmail.com>
2026-01-29 03:42:19 -0300

    router.py é responsavel por aloca as rotas da aplicação.

---
+++ app/routes/router.py
@@ -0,0 +1,42 @@
+from fastapi import FastAPI
+
+
+def register_routes(app: FastAPI) -> None:
+    """
+    Centraliza o registro de todas as rotas da aplicação.
+    """
+
+    # AUTH
+    from app.routes.auth.login import router as login_api_router
+    from app.routes.auth.login import router_login as login_html_router
+    from app.routes.auth.register import router as register_router
+
+    app.include_router(login_api_router)
+    app.include_router(login_html_router)
+    app.include_router(register_router)
+
+    # LANDPAGE
+    from app.routes.landpage import router as landpage_router
+    app.include_router(landpage_router)
+
+    # AGENDAME COMPANY
+    from app.routes.agendame_company.register_services import (
+        router as create_services_router,
+    )
+    from app.routes.agendame_company.agendame_service import (
+        router as adm_services_router,
+    )
+
+    from app.routes.agendame_company.info_company import router as info_company
+
+    # from app.routes.agendame_company.remove_or_upgrad_service import (
+    #     router as remove_service_router,
+
+    from app.routes.agendame_company.appointments import (
+        router as appointments_router,
+    )
+
+    app.include_router(create_services_router)
+    app.include_router(adm_services_router)
+    app.include_router(appointments_router)
+    app.include_router(info_company)
